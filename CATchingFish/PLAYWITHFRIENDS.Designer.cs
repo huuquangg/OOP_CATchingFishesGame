95254f73f6c6a8bbd82e9cfe7ad7040d49a9b9f8
huuquangg <21522508@gm.uit.edu.vn>
2023-11-23 07:55:27 +0700

    Big Update Tien mua giai

--- CATchingFish/PLAYWITHFRIENDS.Designer.cs
+++ CATchingFish/PLAYWITHFRIENDS.Designer.cs
@@ -30,36 +30,44 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PLAYWITHFRIENDS));
             this.Table = new System.Windows.Forms.TableLayoutPanel();
-            this.Plate = new System.Windows.Forms.Panel();
-            this.Bone = new System.Windows.Forms.Panel();
-            this.Fishes = new System.Windows.Forms.Panel();
             this.FirstHandPanel = new System.Windows.Forms.Panel();
             this.SecondHand = new System.Windows.Forms.Panel();
             this.ThirdHand = new System.Windows.Forms.Panel();
             this.FourthHand = new System.Windows.Forms.Panel();
+            this.Plate = new System.Windows.Forms.Panel();
+            this.Bone = new System.Windows.Forms.Panel();
+            this.Fishes = new System.Windows.Forms.Panel();
+            this.Player1Score = new System.Windows.Forms.Button();
+            this.Player2Score = new System.Windows.Forms.Button();
+            this.Player4Score = new System.Windows.Forms.Button();
+            this.Player3Score = new System.Windows.Forms.Button();
             this.FirstLongHand = new System.Windows.Forms.Panel();
             this.SecondLongHand = new System.Windows.Forms.Panel();
             this.ThirdLongHand = new System.Windows.Forms.Panel();
             this.FourthLongHand = new System.Windows.Forms.Panel();
-            this.Player1Score = new System.Windows.Forms.Button();
             this.Table.SuspendLayout();
             this.Plate.SuspendLayout();
-            this.FirstLongHand.SuspendLayout();
             this.SuspendLayout();
             // 
             // Table
             // 
             this.Table.AutoSize = true;
             this.Table.BackColor = System.Drawing.Color.Transparent;
-            this.Table.ColumnCount = 3;
-            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.Table.ColumnCount = 5;
             this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.Table.Controls.Add(this.Plate, 1, 1);
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.Table.Controls.Add(this.FirstHandPanel, 0, 0);
-            this.Table.Controls.Add(this.SecondHand, 2, 0);
+            this.Table.Controls.Add(this.SecondHand, 4, 0);
             this.Table.Controls.Add(this.ThirdHand, 0, 2);
-            this.Table.Controls.Add(this.FourthHand, 2, 2);
+            this.Table.Controls.Add(this.FourthHand, 4, 2);
+            this.Table.Controls.Add(this.Plate, 2, 1);
+            this.Table.Controls.Add(this.Player1Score, 1, 0);
+            this.Table.Controls.Add(this.Player2Score, 3, 0);
+            this.Table.Controls.Add(this.Player4Score, 3, 2);
+            this.Table.Controls.Add(this.Player3Score, 1, 2);
             this.Table.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Table.Location = new System.Drawing.Point(0, 0);
             this.Table.Name = "Table";
@@ -69,6 +77,49 @@
             this.Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.Table.Size = new System.Drawing.Size(971, 595);
             this.Table.TabIndex = 1;
+            // 
+            // FirstHandPanel
+            // 
+            this.FirstHandPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("FirstHandPanel.BackgroundImage")));
+            this.FirstHandPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.FirstHandPanel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FirstHandPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.FirstHandPanel.Location = new System.Drawing.Point(0, 0);
+            this.FirstHandPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.FirstHandPanel.Name = "FirstHandPanel";
+            this.FirstHandPanel.Size = new System.Drawing.Size(194, 198);
+            this.FirstHandPanel.TabIndex = 4;
+            // 
+            // SecondHand
+            // 
+            this.SecondHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SecondHand.BackgroundImage")));
+            this.SecondHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.SecondHand.Dock = System.Windows.Forms.DockStyle.Right;
+            this.SecondHand.Location = new System.Drawing.Point(776, 0);
+            this.SecondHand.Margin = new System.Windows.Forms.Padding(0);
+            this.SecondHand.Name = "SecondHand";
+            this.SecondHand.Size = new System.Drawing.Size(195, 198);
+            this.SecondHand.TabIndex = 5;
+            // 
+            // ThirdHand
+            // 
+            this.ThirdHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ThirdHand.BackgroundImage")));
+            this.ThirdHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ThirdHand.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ThirdHand.Location = new System.Drawing.Point(3, 399);
+            this.ThirdHand.Name = "ThirdHand";
+            this.ThirdHand.Size = new System.Drawing.Size(188, 193);
+            this.ThirdHand.TabIndex = 3;
+            // 
+            // FourthHand
+            // 
+            this.FourthHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("FourthHand.BackgroundImage")));
+            this.FourthHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.FourthHand.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FourthHand.Location = new System.Drawing.Point(779, 399);
+            this.FourthHand.Name = "FourthHand";
+            this.FourthHand.Size = new System.Drawing.Size(189, 193);
+            this.FourthHand.TabIndex = 4;
             // 
             // Plate
             // 
@@ -80,7 +131,6 @@
             this.Plate.Name = "Plate";
             this.Plate.Size = new System.Drawing.Size(188, 192);
             this.Plate.TabIndex = 6;
-            this.Plate.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Bone
             // 
@@ -102,99 +152,103 @@
             this.Fishes.Size = new System.Drawing.Size(188, 192);
             this.Fishes.TabIndex = 0;
             // 
-            // FirstHandPanel
+            // Player1Score
             // 
-            this.FirstHandPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("FirstHandPanel.BackgroundImage")));
-            this.FirstHandPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.FirstHandPanel.Cursor = System.Windows.Forms.Cursors.Default;
-            this.FirstHandPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.FirstHandPanel.Location = new System.Drawing.Point(0, 0);
-            this.FirstHandPanel.Margin = new System.Windows.Forms.Padding(0);
-            this.FirstHandPanel.Name = "FirstHandPanel";
-            this.FirstHandPanel.Size = new System.Drawing.Size(206, 198);
-            this.FirstHandPanel.TabIndex = 4;
-            this.FirstHandPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.FirstHandPanel_Paint);
+            this.Player1Score.BackColor = System.Drawing.Color.Transparent;
+            this.Player1Score.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Player1Score.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player1Score.ForeColor = System.Drawing.Color.Red;
+            this.Player1Score.Location = new System.Drawing.Point(197, 3);
+            this.Player1Score.Name = "Player1Score";
+            this.Player1Score.Size = new System.Drawing.Size(97, 61);
+            this.Player1Score.TabIndex = 7;
+            this.Player1Score.Text = "0";
+            this.Player1Score.UseVisualStyleBackColor = false;
             // 
-            // SecondHand
+            // Player2Score
             // 
-            this.SecondHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SecondHand.BackgroundImage")));
-            this.SecondHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.SecondHand.Dock = System.Windows.Forms.DockStyle.Right;
-            this.SecondHand.Location = new System.Drawing.Point(735, 0);
-            this.SecondHand.Margin = new System.Windows.Forms.Padding(0);
-            this.SecondHand.Name = "SecondHand";
-            this.SecondHand.Size = new System.Drawing.Size(236, 198);
-            this.SecondHand.TabIndex = 5;
+            this.Player2Score.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Player2Score.BackColor = System.Drawing.Color.Transparent;
+            this.Player2Score.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Player2Score.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2Score.ForeColor = System.Drawing.Color.DarkOrange;
+            this.Player2Score.Location = new System.Drawing.Point(676, 3);
+            this.Player2Score.Name = "Player2Score";
+            this.Player2Score.Size = new System.Drawing.Size(97, 61);
+            this.Player2Score.TabIndex = 8;
+            this.Player2Score.Text = "0";
+            this.Player2Score.UseVisualStyleBackColor = false;
             // 
-            // ThirdHand
+            // Player4Score
             // 
-            this.ThirdHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ThirdHand.BackgroundImage")));
-            this.ThirdHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ThirdHand.Dock = System.Windows.Forms.DockStyle.Left;
-            this.ThirdHand.Location = new System.Drawing.Point(3, 399);
-            this.ThirdHand.Name = "ThirdHand";
-            this.ThirdHand.Size = new System.Drawing.Size(203, 193);
-            this.ThirdHand.TabIndex = 3;
+            this.Player4Score.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Player4Score.BackColor = System.Drawing.Color.Transparent;
+            this.Player4Score.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Player4Score.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player4Score.ForeColor = System.Drawing.Color.Blue;
+            this.Player4Score.Location = new System.Drawing.Point(676, 531);
+            this.Player4Score.Name = "Player4Score";
+            this.Player4Score.Size = new System.Drawing.Size(97, 61);
+            this.Player4Score.TabIndex = 9;
+            this.Player4Score.Text = "0";
+            this.Player4Score.UseVisualStyleBackColor = false;
             // 
-            // FourthHand
+            // Player3Score
             // 
-            this.FourthHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("FourthHand.BackgroundImage")));
-            this.FourthHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.FourthHand.Dock = System.Windows.Forms.DockStyle.Right;
-            this.FourthHand.Location = new System.Drawing.Point(730, 399);
-            this.FourthHand.Name = "FourthHand";
-            this.FourthHand.Size = new System.Drawing.Size(238, 193);
-            this.FourthHand.TabIndex = 4;
+            this.Player3Score.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Player3Score.BackColor = System.Drawing.Color.Transparent;
+            this.Player3Score.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Player3Score.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player3Score.ForeColor = System.Drawing.Color.Green;
+            this.Player3Score.Location = new System.Drawing.Point(197, 531);
+            this.Player3Score.Name = "Player3Score";
+            this.Player3Score.Size = new System.Drawing.Size(97, 61);
+            this.Player3Score.TabIndex = 10;
+            this.Player3Score.Text = "0";
+            this.Player3Score.UseVisualStyleBackColor = false;
             // 
             // FirstLongHand
             // 
             this.FirstLongHand.BackColor = System.Drawing.Color.Transparent;
             this.FirstLongHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("FirstLongHand.BackgroundImage")));
             this.FirstLongHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.FirstLongHand.Controls.Add(this.Player1Score);
             this.FirstLongHand.Location = new System.Drawing.Point(3, 0);
             this.FirstLongHand.Name = "FirstLongHand";
-            this.FirstLongHand.Size = new System.Drawing.Size(467, 297);
+            this.FirstLongHand.Size = new System.Drawing.Size(467, 300);
             this.FirstLongHand.TabIndex = 7;
             // 
             // SecondLongHand
             // 
+            this.SecondLongHand.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.SecondLongHand.BackColor = System.Drawing.Color.Transparent;
             this.SecondLongHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SecondLongHand.BackgroundImage")));
             this.SecondLongHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.SecondLongHand.Location = new System.Drawing.Point(498, 0);
+            this.SecondLongHand.Location = new System.Drawing.Point(499, 0);
             this.SecondLongHand.Name = "SecondLongHand";
-            this.SecondLongHand.Size = new System.Drawing.Size(473, 297);
+            this.SecondLongHand.Size = new System.Drawing.Size(472, 300);
             this.SecondLongHand.TabIndex = 7;
             // 
             // ThirdLongHand
             // 
+            this.ThirdLongHand.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.ThirdLongHand.BackColor = System.Drawing.Color.Transparent;
             this.ThirdLongHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ThirdLongHand.BackgroundImage")));
             this.ThirdLongHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ThirdLongHand.Location = new System.Drawing.Point(0, 293);
+            this.ThirdLongHand.Location = new System.Drawing.Point(0, 306);
             this.ThirdLongHand.Name = "ThirdLongHand";
-            this.ThirdLongHand.Size = new System.Drawing.Size(470, 302);
+            this.ThirdLongHand.Size = new System.Drawing.Size(470, 289);
             this.ThirdLongHand.TabIndex = 7;
             // 
             // FourthLongHand
             // 
+            this.FourthLongHand.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.FourthLongHand.BackColor = System.Drawing.Color.Transparent;
             this.FourthLongHand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("FourthLongHand.BackgroundImage")));
             this.FourthLongHand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.FourthLongHand.Location = new System.Drawing.Point(495, 293);
+            this.FourthLongHand.Location = new System.Drawing.Point(499, 306);
             this.FourthLongHand.Name = "FourthLongHand";
-            this.FourthLongHand.Size = new System.Drawing.Size(476, 302);
+            this.FourthLongHand.Size = new System.Drawing.Size(472, 289);
             this.FourthLongHand.TabIndex = 7;
-            // 
-            // Player1Score
-            // 
-            this.Player1Score.Location = new System.Drawing.Point(229, 8);
-            this.Player1Score.Name = "Player1Score";
-            this.Player1Score.Size = new System.Drawing.Size(74, 64);
-            this.Player1Score.TabIndex = 0;
-            this.Player1Score.Text = "0";
-            this.Player1Score.UseVisualStyleBackColor = true;
             // 
             // PLAYWITHFRIENDS
             // 
@@ -211,10 +265,8 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PLAYWITHFRIENDS";
             this.Text = "PLAY WITH FRIENDS";
-            this.Load += new System.EventHandler(this.PLAYWITHFRIENDS_Load);
             this.Table.ResumeLayout(false);
             this.Plate.ResumeLayout(false);
-            this.FirstLongHand.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,7 +276,6 @@
 
         private System.Windows.Forms.TableLayoutPanel Table;
         private System.Windows.Forms.Panel ThirdHand;
-        private System.Windows.Forms.Panel FourthHand;
         private System.Windows.Forms.Panel SecondHand;
         private System.Windows.Forms.Panel FirstHandPanel;
         private System.Windows.Forms.Panel Plate;
@@ -234,7 +285,11 @@
         private System.Windows.Forms.Panel SecondLongHand;
         private System.Windows.Forms.Panel ThirdLongHand;
         private System.Windows.Forms.Panel FourthLongHand;
+        private System.Windows.Forms.Panel FourthHand;
         private System.Windows.Forms.Button Player1Score;
+        private System.Windows.Forms.Button Player2Score;
+        private System.Windows.Forms.Button Player4Score;
+        private System.Windows.Forms.Button Player3Score;
     }
 }
 
--- CATchingFish/PLAYWITHFRIENDS.cs
+++ CATchingFish/PLAYWITHFRIENDS.cs
@@ -9,14 +9,19 @@ namespace CATchingFish
         private Timer panelTimer; // Timer to make the panel invisible after 2 seconds
         private Timer FillFishesInthePlate; // Timer to fill the plate with fishes
         private Timer NoFishesInthePlate; // Timer to make the fishes disappear from the plate
-        private int Player1_score = 0; // Score of the player 1
+
+        private static int Player1_score = 0; // Score of the player 1
+        private static int Player2_score = 0; // Score of the player 2
+        private static int Player3_score = 0; // Score of the player 3
+        private static int Player4_score = 0; // Score of the player 4
+
         public PLAYWITHFRIENDS()
         {
             InitializeComponent();
 
             // Add the timer and set its properties
             panelTimer = new Timer();
-            panelTimer.Interval = 1000;
+            panelTimer.Interval = 300;
 
             // Add the event handler for the timer
             panelTimer.Tick += PanelTimer_Tick;
@@ -31,9 +36,13 @@ namespace CATchingFish
             Bone.Visible = false;
             Fishes.Visible = false;
 
+
             FillFoodOnPlate();
+
             // auto Disappear the fishes in the plate after a random time from 2 to 3 seconds
             DisappearFoodOnPlate();
+
+            // check if the player has 3 fishes, then he/she wins and show the winner message and stop the game
         }
         // Play background music
         private void LoopMusic()
@@ -45,9 +54,9 @@ namespace CATchingFish
         protected void FillFoodOnPlate()
         {
             FillFishesInthePlate = new Timer();
-            FillFishesInthePlate.Interval = generateRandomNumber();
-            FillFishesInthePlate.Tick += DisplayFishesOrBone;
-            FillFishesInthePlate.Start();
+            FillFishesInthePlate.Interval = generateRandomNumber(); // generate a random number from 2 to 3 seconds
+            FillFishesInthePlate.Tick += DisplayFishesOrBone; // event handler
+            FillFishesInthePlate.Start();// start the timer
         }
         protected void DisappearFoodOnPlate()
         {
@@ -56,19 +65,9 @@ namespace CATchingFish
             NoFishesInthePlate.Tick += Disappear;
             NoFishesInthePlate.Start();
         }
-        // Check collision between the hand and the food
-        protected int CatfishOrBone()
-        {
-            if (FirstHandPanel.Bounds.IntersectsWith(Fishes.Bounds))
-            {
-                return 1;
-            }    
-            else if (FirstHandPanel.Bounds.IntersectsWith(Bone.Bounds))
-            {
-                return -1;
-            }
-            return 0;
-        }   
+
+        // check if the player has 3 fishes, then he/she wins and show the winner message and stop the game
+
 
         // Event handler when pressing the Q key (First Player)
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
@@ -77,15 +76,10 @@ namespace CATchingFish
             {
                 // Timer event handler - make the panel invisible when the timer ticks
                 FirstHandPanel.Visible = false;
-                if (FirstHandPanel.Visible == false) FirstLongHand.Visible = true;
+                FirstLongHand.Visible = true;
 
-/*                // Code duoi la psudocode de mota logic cong/ tru diem
-                if (FirstLongHand.Visible && FillFishesInthePlate.Enabled && Fishes.Visible)
-                    // Score++
-                    DisappearFoodOnPlate();
-                if (FirstLongHand.Visible && FillFishesInthePlate.Enabled && !Fishes.Visible)
-                    // Score--
-                    DisappearFoodOnPlate();*/
+                ScoreControl(ref Player1_score, Player1Score);
+
                 panelTimer.Start();
                 return true;
             }
@@ -94,7 +88,10 @@ namespace CATchingFish
             {
                 // Timer event handler - make the panel invisible when the timer ticks
                 SecondHand.Visible = false;
-                if (SecondHand.Visible == false) SecondLongHand.Visible = true;
+                SecondLongHand.Visible = true;
+
+                ScoreControl(ref Player2_score, Player2Score);
+
                 panelTimer.Start();
                 return true;
             }
@@ -103,7 +100,10 @@ namespace CATchingFish
             {
                 // Timer event handler - make the panel invisible when the timer ticks
                 ThirdHand.Visible = false;
-                if (ThirdHand.Visible == false) ThirdLongHand.Visible = true;
+                ThirdLongHand.Visible = true;
+
+                ScoreControl(ref Player3_score, Player3Score);
+
                 panelTimer.Start();
                 return true;
             }
@@ -112,9 +112,24 @@ namespace CATchingFish
             {
                 // Timer event handler - make the panel invisible when the timer ticks
                 FourthHand.Visible = false;
-                if (FourthHand.Visible == false) FourthLongHand.Visible = true;
+                FourthLongHand.Visible = true;
+
+                ScoreControl(ref Player4_score, Player4Score);
+
                 panelTimer.Start();
                 return true;
+            }
+
+            if (keyData == Keys.Escape)
+            {
+                DialogResult result;
+                result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (result == DialogResult.Yes)
+                {
+                    Close();
+                }
+                return false;
             }
 
 
@@ -124,23 +139,7 @@ namespace CATchingFish
         // Timer event handler
         private void PanelTimer_Tick(object sender, EventArgs e)
         {
-            if (CatfishOrBone() == 1)
-            {
-                // Score++
-                Player1_score++;
-                Player1Score.Text = Player1_score.ToString();
-            }
-            else if (CatfishOrBone() == -1)
-            {
-                // Score--
-                Player1_score--;
-                Player1Score.Text = Player1_score.ToString();
-            }
-            else
-            {
-                // Do nothing
-            }
-            
+
             panelTimer.Stop(); // Stop the timer
             HandControl(FirstHandPanel, FirstLongHand);
             HandControl(SecondHand, SecondLongHand);
@@ -148,26 +147,43 @@ namespace CATchingFish
             HandControl(FourthHand, FourthLongHand);
         }
 
+        protected void ScoreControl(ref int PlayerGOTFish, Button ScoreBoard)
+        {
+            // Check FillFishesInThePlate timer is running or DisappearFoodOnPlate timer is running
+            // If FillFishesInThePlate timer is running, then score++
+            // If DisappearFoodOnPlate timer is running, then score--
+            if (!FillFishesInthePlate.Enabled && Fishes.Visible)
+            {
+                // Score++
+                PlayerGOTFish++;
+                ScoreBoard.Text = PlayerGOTFish.ToString();
+
+                // Start the timer to fill the plate with fishes again
+                FillFishesInthePlate.Start();
+                Fishes.Visible = false;
+            }
+            else if (!FillFishesInthePlate.Enabled && !Fishes.Visible)
+            {
+                // Score--
+                PlayerGOTFish--;
+                if (PlayerGOTFish < 0) ScoreBoard.Text = "-" + (Math.Abs(PlayerGOTFish).ToString());
+                else ScoreBoard.Text = PlayerGOTFish.ToString();
+                FillFishesInthePlate.Start();
+                Bone.Visible = false;
+            }
+            else
+            {
+                // Do nothing
+            }
+
+        }
         protected void HandControl(Panel Short_Hand, Panel Long_Hand)
         {
             Short_Hand.Visible = true;
             Long_Hand.Visible = false;
-        }
-        private void PLAYWITHFRIENDS_Load(object sender, EventArgs e)
-        {
 
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        public bool isFishOnPlate()
-        {
-            if (Fishes.Visible == true) return true;
-            return false;
-        }
 
         private void DisplayFishesOrBone(object sender, EventArgs e)
         {
@@ -208,15 +224,9 @@ namespace CATchingFish
         {
             Random rnd = new Random();
             object[] Food = { Fishes, Bone };
-            int num = rnd.Next(0, 2);
+            int num = rnd.Next(Food.Length);
             return Food[num];
         }
-
-        private void FirstHandPanel_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
 
     }
 }
